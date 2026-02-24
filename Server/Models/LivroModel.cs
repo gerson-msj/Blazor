d377f96feff60aa4216485a751f50ad544305d10
gerson-msj <gerson.msj@gmail.com>
2026-02-23 22:20:53 -0300

    Livro Edit

--- Server/Models/LivroModel.cs
+++ Server/Models/LivroModel.cs
@@ -8,6 +8,17 @@ public class LivroModel : BaseLivroModel
     [Required(ErrorMessage = "Informe o {0}.")]
     public SelectItemModel? Autor { get; set; }
 
+    public LivroModel() { }
+
+    public LivroModel(LivroDto dto)
+    {
+        Id = dto.Id;
+        Titulo = dto.Titulo;
+        Autor = new(dto.Autor.Id, dto.Autor.Nome);
+        DataConclusao = dto.DataConclusao;
+        Comentarios = dto.Comentarios;
+    }
+
     public LivroDto ToDto() => new()
     {
         Id = Id,
